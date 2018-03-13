16a27d02be1d95eff64d12e7138508ed75270a5d
BIT Student <classroom@bgit.rs>
2018-03-13 16:59:21 +0100

    zzz

---
+++ Week7/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    // res.setHeader("Content-type", "application/JSON");
+    res.setHeader("Content-type", "application/JSON");
+    // res.end("<h1>Hello world from node server</h>");
+    res.end(JSON.stringify({msg: "Hello", name: "zzz", pare: "200 miliJona"}))
+});
+
+server.listen(port, hostname, () => {
+    console.log("Server running at http://" + hostname + ":" + port);
+    
+});
